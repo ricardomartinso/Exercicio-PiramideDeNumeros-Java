beb774dc69331ec03c7f75e73189c8a3f3f201af
ricardomartinso <ricardomartinso.dev@gmail.com>
2023-01-10 11:59:27 -0300

    feat: piramide de 5 completed

--- src/PiramideDeNumeros.java
+++ src/PiramideDeNumeros.java
@@ -1,5 +1,13 @@
 public class PiramideDeNumeros {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        for (int i = 1; i < 6; i++) {
+
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
+            }
+            System.out.println("");
+        }
+
     }
 }
